9e46fc389423655e15849f7d8adf09c174b00291
101ta28 <workta28@gmail.com>
2023-04-24 11:28:22 +0900

    remove user_name and add instagram link

--- src/components/ContactComp.vue
+++ src/components/ContactComp.vue
@@ -11,7 +11,7 @@
           <v-card class="mx-auto clickable" max-width="600" @click="openLink(contact.link)">
             <v-card-title primary-title class="d-flex align-center justify-center">
               <v-icon class="text-h4">{{ contact.icon }}</v-icon>
-              <h4 class="text-h5 text-center">{{ contact.title }} : {{ contact.user_name }}</h4>
+              <h4 class="text-h5 text-center">{{ contact.title }}</h4>
             </v-card-title>
           </v-card>
         </v-col>
@@ -24,8 +24,9 @@
 import { ref } from 'vue';
 
 const contactList = ref([
-  { title: 'Twitter', icon: 'mdi-twitter', user_name: '101ta28', link: 'https://twitter.com/101ta28' },
-  { title: 'GitHub', icon: 'mdi-github', user_name: '101ta28', link: 'https://github.com/101ta28' },
+  { title: 'Twitter', icon: 'mdi-twitter', link: 'https://twitter.com/101ta28' },
+  { title: 'GitHub', icon: 'mdi-github', link: 'https://github.com/101ta28' },
+  { title: 'Instagram', icon: 'mdi-instagram', link: 'https://www.instagram.com/101ta28/' },
 ])
 
 const openLink = (link) => {
